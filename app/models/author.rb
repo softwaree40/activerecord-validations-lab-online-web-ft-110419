f906d5e4a5fd7f06e193084b8f5c89ef8cf12dea
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-04 22:49:53 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,8 @@
 class Author < ActiveRecord::Base
       validates :name, presence: true
+
       validates :phone_number, length: {minimum: 10 }
       validates :name, uniqueness: true
-      
+  
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ActiveRecord::Base
    validates :content, length: { minimum: 250 }
    validates :summary, length: { maximum: 250 }
    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+   validate :clickbait
+   def clickbait
+        if title == nil 
+          errors.add(:title, "This is not clickbait")
+        
+         elsif !title.match(/Won\'t Believe|Secret|Top [0-9]|Guess/)
+           errors.add(:title, "This is not clickbait")
+       end
+   end
 end
