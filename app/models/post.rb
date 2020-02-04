7f435b5b845b8083708c99c62bcc25ec1c6040c6
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-04 22:04:31 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
    validates :title, presence: true
+<<<<<<< HEAD
    validates :content, length: { minimum: 250 }
    validates :summary, length: { maximum: 250 }
    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+=======
+>>>>>>> 5950ea001f1791409aebc48b79ca0540481effec
 end
