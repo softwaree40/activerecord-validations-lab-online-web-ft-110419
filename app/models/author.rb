dee978d55e6db6c64e0f8954175ee2c028cc8202
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-04 22:07:39 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,12 +1,8 @@
 class Author < ActiveRecord::Base
       validates :name, presence: true
-<<<<<<< HEAD
+
       validates :phone_number, length: {minimum: 10 }
       validates :name, uniqueness: true
-      
-=======
-      validates :phone_number, length: { minimum: 10 }
-      validates :name, uniqueness: true
   
->>>>>>> 5950ea001f1791409aebc48b79ca0540481effec
+
 end
